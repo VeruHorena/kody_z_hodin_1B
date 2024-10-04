51e233a3b93d476fe50d9f3c5b74dffb9fe51d7c
VeruHorena <horenovska.veronika@gmail.com>
2024-10-04 12:57:07 +0200

    hodina 4_10

---
+++ kod_hodina4_10/kod_hodina4_10/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace kod_hodina4_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Zadej cislo:");
+             int cislo = int.Parse(Console.ReadLine());
+             if (cislo >= 10)
+             {
+                 Console.WriteLine("Vetsi nez 10");
+             }
+             else
+             {
+                 Console.WriteLine("Neni vetsi nez 10");
+
+             }
+            */
+            Console.WriteLine("Zadej text:");
+            string text = Console.ReadLine();
+            text = text.ToLower(); //prevod na mala pismena
+            if(/*(text == "Ahoj")||(text=="ahoj")*/ text=="ahoj")
+            {
+                Console.WriteLine("Text byl ahoj");
+            }
+            else
+            {
+                Console.WriteLine("Text byl cau");
+            }
+        }
+    }
+}
