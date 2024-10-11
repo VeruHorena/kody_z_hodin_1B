f037f6d1de3b016f1a2d3ba3d32bbe4159d1771f
VeruHorena <horenovska.veronika@gmail.com>
2024-10-11 12:14:29 +0200

    denTydne

---
+++ den_tydne - ok/den_tydne/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Zadej poradove cislo dne:");
+        int poradove = int.Parse(Console.ReadLine());
+        string nazev = "";
+
+        switch (poradove) {
+            case 1:
+                nazev = "pondeli";
+                break;
+            case 2:
+                nazev = "utery";
+                break;
+            case 3:
+                nazev = "streda";
+                break;
+            case 4:
+                nazev = "ctvrtek";
+                break;
+            case 5:
+                nazev = "patek";
+                break;
+            case 6:
+                nazev = "sobota";
+                break;
+            case 7:
+                nazev = "nedele";
+                break;
+            default:
+                Console.WriteLine("Hodnota neni platna.");              
+                break;
+        }
+        if (nazev != "")
+            Console.WriteLine("Nazev dne je: " + nazev);
+ 
+
+    }
+}
