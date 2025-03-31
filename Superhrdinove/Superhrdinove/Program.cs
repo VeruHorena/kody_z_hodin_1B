f6903927a8837596924a592d3f6065c3965eb1f1
VeruHorena <horenovska.veronika@gmail.com>
2025-03-31 08:11:12 +0200

    superhrdina

---
+++ Superhrdinove/Superhrdinove/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Superhrdinove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Superhrdina[] superhrdinove = new Superhrdina[2];
+            int pocetHrdinu = 0;
+
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1 - Vytvořit superhrdinu");
+                Console.WriteLine("2 - Zobrazit informace o superhrdinech");
+                Console.WriteLine("3 - Trénovat superhrdinu");
+                Console.WriteLine("4 - Útok superhrdiny");
+                Console.WriteLine("5 - Ukončit");
+                Console.Write("Vyberte možnost: ");
+
+                string volba = Console.ReadLine();
+
+                switch (volba)
+                {
+                    case "1":
+                        if (pocetHrdinu < 2)
+                        {
+                            Console.Write("Zadejte jméno superhrdiny: ");
+                            string jmeno = Console.ReadLine();
+                            Console.Write("Zadejte superschopnost: ");
+                            string schopnost = Console.ReadLine();
+
+                            superhrdinove[pocetHrdinu] = new Superhrdina(jmeno, schopnost);
+                            pocetHrdinu++;
+                            Console.WriteLine("Superhrdina úspěšně vytvořen!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Maximální počet superhrdinů byl vytvořen!");
+                        }
+                        break;
+
+                    case "2":
+                        foreach (var hrdina in superhrdinove)
+                        {
+                            hrdina?.ShowInfo();
+                        }
+                        break;
+
+                    case "3":
+                        Console.Write("Vyberte superhrdinu k tréninku (1 nebo 2): ");
+                        if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= pocetHrdinu)
+                        {
+                            Console.Write("Zadejte hodnotu tréninku: ");
+                            if (int.TryParse(Console.ReadLine(), out int hodnota))
+                            {
+                                superhrdinove[index - 1].Train(hodnota);
+                            }
+                        }
+                        break;
+
+                    case "4":
+                        if (pocetHrdinu < 2)
+                        {
+                            Console.WriteLine("Nejsou dostateční hrdinové k útoku!");
+                        }
+                        else
+                        {
+                            superhrdinove[0].Attack(superhrdinove[1]);
+                        }
+                        break;
+
+                    case "5":
+                        return;
+
+                    default:
+                        Console.WriteLine("Neplatná volba, zkuste to znovu.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Superhrdinove/Superhrdinove/Superhrdina.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhrdinove
+{
+    class Superhrdina
+    {
+        private string jmeno;
+        private int urovenSily;
+        private int zdravi;
+        private string superschopnost;
+
+        public Superhrdina(string jmeno, string superschopnost)
+        {
+            this.jmeno = jmeno;
+            this.superschopnost = superschopnost;
+            Random rnd = new Random();
+            this.urovenSily = rnd.Next(1, 101);
+            this.zdravi = rnd.Next(1, 101);
+        }
+
+        public string GetJmeno()
+        {
+            return jmeno;
+        }
+
+        public void Attack(Superhrdina s)
+        {
+            Console.WriteLine($"{jmeno} útočí na {s.GetJmeno()} svou schopností {superschopnost}!");
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Jsem {jmeno} a moje superschopnost je {superschopnost}!");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{jmeno} - Zdraví: {zdravi}, Síla: {urovenSily}");
+        }
+
+        public void Train(int hodnota)
+        {
+            urovenSily += hodnota;
+            zdravi -= 10;
+            if (zdravi < 0) zdravi = 0;
+            Console.WriteLine($"{jmeno} trénoval a nyní má sílu {urovenSily}, ale jeho zdraví kleslo na {zdravi}.");
+        }
+    }
+
+
+}
